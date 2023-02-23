192182239fc7b30ed60a59753170d98b8d9a7928
root <root@virt155.devlab.pgu1.suse.com>
2023-02-23 05:49:01 -0700

    fix adding data to xml file

--- src/virtscenario/xmlutil.py
+++ src/virtscenario/xmlutil.py
@@ -67,11 +67,13 @@ def add_attestation(file, godh, session):
     # python >= 3.9
     #ET.indent(root, space='    ', level=0)
     dhcert = ET.SubElement(lsdef, 'dhCert')
-    dhcert.text = open(godh).read()
+    gtext = open(godh).read()
+    dhcert.text = gtext #open(godh).read()
     dhcert.tail = "\n    "
-    session = ET.SubElement(lsdef, 'session')
-    session.text = open(session).read()
-    session.tail = "\n  "
+    sessionel = ET.SubElement(lsdef, 'session')
+    stext = open(session).read()
+    sessionel.text = stext #open(session).read()
+    sessionel.tail = "\n  "
     ET.ElementTree(root).write(file)
 
 def show_tag(root, child):
