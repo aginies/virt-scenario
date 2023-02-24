9efe20f1f284892d21d737b89c6325de2a8ed945
aginies <aginies@suse.com>
2023-02-24 10:54:43 +0100

    start pylint cleanup

--- src/virtscenario/libvirt.py
+++ src/virtscenario/libvirt.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import xml.etree.ElementTree as ET
-import virtscenario.util as util
 
 class FeatureNotSupported(BaseException):
     def __init(__self__):
