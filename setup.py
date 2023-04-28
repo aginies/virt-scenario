14c5fe939cb6934e6b34651d80650444a805a833
aginies <aginies@suse.com>
2023-04-28 15:34:03 +0200

    add a binairie for the GTK

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ setuptools.setup(
             "virt-scenario = virtscenario.main:main",
             "virt-scenario-launch = virtscenario_launch.main:main",
             "virt-select-firmware = virt_select_firmware.main:main",
+            "virt-scenario-gtk = gtk.main:main",
         ]
     },
     classifiers=[
